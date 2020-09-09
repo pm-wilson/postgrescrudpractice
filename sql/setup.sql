f6bdf1652a219470b4b553f65945b53e0b8578b3
Patrick Wilson <pmwilson333@gmail.com>
2020-09-08 17:19:21 -0700

    deck insert working

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS plants;
+DROP TABLE IF EXISTS decks;
 
 CREATE TABLE plants (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -7,3 +8,11 @@ CREATE TABLE plants (
   water_freq INT,
   plant_description TEXT
 );
+
+CREATE TABLE decks (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  player TEXT,
+  colors VARCHAR(5),
+  format TEXT,
+  rank INT
+)
